b53e95266573e303b65cfc8b337cefb403ab0468
iimoon <aymanabdk@gmail.com>
2024-01-23 09:54:33 +0530

    updated login design

--- mentalhealth-main/src/Components/loginpage/Login.jsx
+++ mentalhealth-main/src/Components/loginpage/Login.jsx
@@ -1,81 +1,175 @@
-import React, { useState } from 'react';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from 'axios';
-import './login.css';
 import { useNavigate } from 'react-router-dom';
+import { MenuItem } from '@mui/material';
+
+const theme = createTheme();
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        CareGroove
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 const Login = () => {
   const navigate = useNavigate();
-  const [loginData, setLoginData] = useState({
+  const [loginData, setLoginData] = React.useState({
     email: '',
     password: '',
-    usertype: '', // Add usertype field
+    usertype: '',
   });
-
-  const [notification, setNotification] = useState(null);
-
   const inputHandler = (e) => {
     const { name, value } = e.target;
     setLoginData((prevData) => ({ ...prevData, [name]: value }));
   };
-
-  const loginHandler = () => {
-    // Assuming you have separate routes for admin and user login
-    const loginRoute =
-      loginData.usertype === 'admin'
-        ? 'http://localhost:3007/admin/login'
-        : 'http://localhost:3007/user/login';
-
-    axios
-      .post(loginRoute, loginData, {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const email = data.get('email');
+    const password = data.get('password');
+    const loginRoute = 
+     loginData.usertype === 'admin'
+     ? 'http://localhost:3007/admin/login'
+     : 'http://localhost:3007/user/login'
+    try {
+      const response = await axios.post(loginRoute, {
+        email,
+        password,
+        usertype: '', // Assuming no usertype for user login
+      }, {
         headers: { 'Content-Type': 'application/json' },
-      })
-      .then((response) => {
-        // Handle successful login
-        const { token } = response.data;
-        localStorage.setItem('jwtToken', token);
-        const adminpath = '/admin';
-        navigate(adminpath);
-        console.log(response.data.message);
-        console.log(response.data.data); // This will contain user/admin data
-      })
-      .catch((error) => {
-        // Handle login error
-        console.log('Error:', error);
-        setNotification('Invalid credentials. Please try again.'); // Set the error notification
-        setTimeout(() => {
-          setNotification(null); // Clear the notification after a few seconds
-        }, 5000);
       });
+
+      const { token } = response.data;
+      localStorage.setItem('jwtToken', token);
+      const adminpath = '/admin'; // Adjust the path as needed
+      navigate(adminpath);
+      console.log(response.data.message);
+      console.log(response.data.data);
+    } catch (error) {
+      console.error('Error:', error);
+      // Adjust error handling as needed
+    }
   };
 
   return (
-    <div className="main">
-      <div className="intro">
-        <h3>Login</h3>
-        <p>It's quick and easy.</p>
-      </div>
-      <div className="form-container">
-        <div className="form">
-          <input type="text" name="email" placeholder="Email address" onChange={inputHandler} />
-          <input type="password" name="password" placeholder="Password" onChange={inputHandler} />
-          <select className="usertype" name="usertype" onChange={inputHandler}>
-            <option value="" disabled selected>
-              Select UserType
-            </option>
-            <option value="user">User</option>
-            <option value="admin">Admin</option>
-          </select>
-          <input type="button" id="button" value="Login" onClick={loginHandler} />
-        </div>
-      </div>
-      <div className="logo-end">
-        <h2>CareGroove</h2>
-        <p>Your mental health assistant.</p>
-      </div>
-      <div className="notification">
-        {notification && <p>{notification}</p>}
-      </div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="usertype"
+                label="User Type"
+                select
+                value={loginData.usertype}
+                onChange={inputHandler}
+              >
+                <MenuItem value="user">User</MenuItem>
+                <MenuItem value="admin">Admin</MenuItem>
+              </TextField>
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 };
 
